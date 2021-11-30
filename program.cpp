207d59255c5ae59bf472a5e60a1d7076cbba395f
Sam Smirnov <smirnovsema@gmail.com>
2021-11-30 06:07:36 +0300

    Snapshot1, program.cpp

---
+++ program.cpp
@@ -0,0 +1,12 @@
+//файл program.cpp
+#include "stdafx.h"
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main()
+{
+cout << MySum(10, 3) << endl;
+cout << MySub(10, 3) << endl;
+
+return 0;
+}
